e366383219989db085ee277bf0da5d507184b04f
Eduardo Gamboa <eduargary93@gmail.com>
2019-11-19 16:32:22 -0600

    act

---
+++ Dana/capaPresentacionn/Ventas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capaPresentacionn
+{
+    public partial class Ventas : Form
+    {
+        public Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Id_Cliente_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuscarCliente_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
